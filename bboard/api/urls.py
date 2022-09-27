0032686c6a3856ad2a4db99a19dfb598f4d3838b
gr411_koade <koromislo.molodec@gmail.com>
2022-09-27 14:11:58 +0700

    методичка все. дальше бога нет

--- bboard/api/urls.py
+++ bboard/api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import bbs
+from .views import bbs, BbDetailView
+from .views import comments
 
 urlpatterns = [
+   path('bbs/<int:pk>/', BbDetailView.as_view()),
+   path('bbs/<int:pk>/comments/', comments),
    path('bbs/', bbs),
 ]
